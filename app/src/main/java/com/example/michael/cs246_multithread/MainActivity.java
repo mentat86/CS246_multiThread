a7e032b61d746a250106dabde646d7718027bfee
mentat86 <YamiArchangel@gmail.com>
2015-12-11 18:33:06 -0700

    Initial commit

---
+++ app/src/main/java/com/example/michael/cs246_multithread/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.michael.cs246_multithread;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
